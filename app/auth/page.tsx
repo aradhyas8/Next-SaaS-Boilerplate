610a77d7487ebb492fd1c3fdce3bd5d227a93867
Aradhya <aradhya@my.yorku.ca>
2024-03-09 12:52:47 -0500

    Initial

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,9 +1,24 @@
+import { Button } from '@/components/ui/button'
+import { KeyRound } from 'lucide-react'
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa6";
 import React from 'react'
 
 function page() {
   return (
-    <div className='flex items-center justify-cen<ter w-full h-screen'>
-        
+    <div className='flex items-center justify-center w-full h-screen'>
+        <div className='w-96 h-96 rounded-md border p-5'>
+            <div className='flex items-center gap-2'>
+                <KeyRound />
+                  <h1 className='text-2xl font-bold'>Next+Supabase</h1>
+            </div>
+            
+              <p className='text-sm text-gray-300'>Register/SignIn Today</p>
+              <div className='flex flex-col gap-5'>
+                  <Button className='w-full flex items-center gap-2' variant='outline'><FaGithub />GitHub</Button>
+                  <Button className='w-full flex items-center gap-2' variant='outline'><FcGoogle />Google</Button>
+              </div>
+        </div>
     </div>
   )
 }
