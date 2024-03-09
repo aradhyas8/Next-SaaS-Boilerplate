e82949dcc8b3507db32267923af84ab23ba78b9f
Aradhya <aradhya@my.yorku.ca>
2024-03-08 22:25:52 -0500

    Initial

---
+++ app/auth/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='flex items-center justify-cen<ter w-full h-screen'>
+        
+    </div>
+  )
+}
+
+export default page
